a346166cb3bdb5b6266bf712bc6e41dce39155c3
Tim Lemke <timlemkeguitarist@gmail.com>
2017-09-11 17:11:38 -0400

    Worked on sorting the cue

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -13,10 +13,12 @@ class App extends React.Component {
 		this.state = {
 			userName: '',
 			temperature: '',
+			hangerLevel: '',
 			userMessage: '',
 			cuedPerson: [],
 			cuedGifs: [],
 			chosenGif: '',
+			// cuePositon: '',
 			user: null,
 		};
 		this.handleChange = this.handleChange.bind(this);
@@ -54,20 +56,27 @@ class App extends React.Component {
 			.then(() => {
 				this.setState({
 					user: null,
+					cuedGifs: [],
 				});
 			});
-
 	}
-
 
 	handleSubmit(event) {
 		event.preventDefault();
 		const newPerson = {
-			temperature: this.state.temperature,
+			temperature: Number(this.state.temperature),
+			hangerLevel: Number(this.state.hangerLevel),
 			user: this.state.user.displayName || this.state.user.email,
 			userMessage: this.state.userMessage,
 			chosenGif: this.state.chosenGif,
 		};
+
+		newPerson.cuePosition = newPerson.temperature + newPerson.hangerLevel;
+
+		// if(newPerson.temperature === "RoomTemp"){
+		// 	newPerson.cuePosition = 1;
+		// } 
+
 		console.log('newPerson', newPerson)
 		dbRef.push(newPerson);
 	}
@@ -106,10 +115,15 @@ class App extends React.Component {
 
 	handleChange(event) {
 		this.setState({
-			[event.target.name]: event.target.value
+			[event.target.name]: event.target.value,
 		});
+		this.setState({
+
+		})
 		
 	}
+
+
 
 	componentDidMount() {
 		dbRef.on('value', (snapshot) => {
@@ -131,12 +145,17 @@ class App extends React.Component {
 				});
 			}
 		});
+
 	}
 
 	render() {
-		console.log('this.state.cuedPerson', this.state.cuedPerson);
-		let sortedCuedPerson = this.state.cuedPerson;
-		// console.log('sortedCuedPerson', s)
+		let sortedCuedPerson = Array.from(this.state.cuedPerson);
+		sortedCuedPerson.sort(function(a, b) {
+			return (a.cuePosition - b.cuePosition);
+		})
+		// console.log(sortedCuedPerson);
+
+
 		return (
 			<div className='app'>
 				<header>
@@ -158,28 +177,37 @@ class App extends React.Component {
 								
 								<img className="profileImage" src="../../public/assets/logo-hackeryou.svg" alt=""/>
 							}
-
 						</div>
 					</div>
 				</header>
 				{this.state.user ?
 				<main>
-					{console.log(this.state.user.photoURL)}
 					<div className="wrapper mainContainer">
 						<section className="addUser">
 							<form onSubmit={this.handleSubmit}>
 								<input className="username" type="text" name="userName" placeholder="What's your name?" value={this.state.user.displayName || this.state.user.email} />
 								<p className="howCold">How Cold Is Your Food?</p>
-								<label htmlFor="roomTemp">Room Temp.</label>
-								<input onClick={this.handleChange} type="radio" name="temperature" value="Room Temp" id="roomTemp"/>
-								<label htmlFor="cool">Cool</label>
-								<input onClick={this.handleChange} type="radio" name="temperature" value="Cool" id="cool"/>
-								<label htmlFor="cold">Cold</label>
-								<input onClick={this.handleChange} type="radio" name="temperature" value="Cold" id="cold"/>
-								<label htmlFor="thawed">Thawed</label>
-								<input onClick={this.handleChange} type="radio" name="temperature" value="Thawed" id="thawed"/>
-								<label htmlFor="frozen">Frozen</label>
-								<input onClick={this.handleChange} type="radio" name="temperature" value="Frozen" id="frozen"/>
+								<label htmlFor="RoomTemp">Room Temp.</label>
+								<input onClick={this.handleChange} type="radio" name="temperature" value={1} id="RoomTemp"/>
+								<label htmlFor="Cool">Cool</label>
+								<input onClick={this.handleChange} type="radio" name="temperature" value={2} id="Cool"/>
+								<label htmlFor="Cold">Cold</label>
+								<input onClick={this.handleChange} type="radio" name="temperature" value={3} id="Cold"/>
+								<label htmlFor="Thawed">Thawed</label>
+								<input onClick={this.handleChange} type="radio" name="temperature" value={4} id="Thawed"/>
+								<label htmlFor="Frozen">Frozen</label>
+								<input onClick={this.handleChange} type="radio" name="temperature" value={5} id="Frozen"/>
+								<p className="howCold">How hangry are you?</p>
+								<label htmlFor="Low">Low</label>
+								<input onClick={this.handleChange} type="radio" name="hangerLevel" value={-1} id="Low"/>
+								<label htmlFor="Beginning">Beginning</label>
+								<input onClick={this.handleChange} type="radio" name="hangerLevel" value={-2} id="Beginning"/>
+								<label htmlFor="frozen">Moderate</label>
+								<input onClick={this.handleChange} type="radio" name="hangerLevel" value={-3} id="Moderate"/>
+								<label htmlFor="Warning">Warning</label>
+								<input onClick={this.handleChange} type="radio" name="hangerLevel" value={-4} id="Warning"/>
+								<label htmlFor="Extreme">Extreme</label>
+								<input onClick={this.handleChange} type="radio" name="hangerLevel" value={-5} id="Extreme"/>
 								<textarea className="usermessage" name="userMessage" cols="30" rows="10" placeholder="What's your message to the group?"  onChange={this.handleChange}></textarea>
 								<input className="giphyQuery" type="text" onChange={this.handleChange} name="giphyQuery"/>
 								<button className="gifSearchButton"onClick={this.searchGiphy}>Gif Me!</button>
@@ -196,15 +224,17 @@ class App extends React.Component {
 						</section>
 						<section className="cueResults">
 							<ul>
-								{this.state.cuedPerson.map((item, id) => {
+								{sortedCuedPerson.map((item, id) => {
 									return(
 										<li key={item.id}>
 											<div className="cueItem">
+												<img className="profileImage" src={this.state.user.photoURL} alt="User Photo"/>
 												<div className="cueText">
 													<h3>{item.user}</h3>
 													<p>Food Temperature: {item.temperature}</p>
 													<p>{item.userMessage}</p>
-													<button onClick={() => this.removeCue(item.id)}>Remove Item</button>
+													{item.user === this.state.user.displayName || item.user === this.state.user.email ?
+													  <button onClick={() => this.removeCue(item.id)}>Remove Item</button> : null}
 												</div>
 												<div className="cueGif">
 													<img src={`${item.chosenGif}`} alt="The users selected gif"/>
